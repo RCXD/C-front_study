15ebd5103c1b5c9858f8aa8bff0a678177ac1f9a
tpxdkss <tpxdkss@gmail.com>
2025-09-09 17:38:50 +0900

    backup

---
+++ 0710/8. 다크모드만들기/script.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector('.btn-mode');
+const body = document.body;
+
+// 다크모드이면 true, 아니면 false
+let isDark = false;
+
+btn.addEventListener('click', () => {
+
+  if(isDark) {
+    body.style.background = 'white';
+    body.style.color = 'black';
+    btn.value = '다크모드';
+    isDark = !isDark;
+  } else {
+    body.style.background = 'black';
+    body.style.color = 'white';
+    btn.value = '라이트모드';
+    isDark = !isDark;
+  }
+  
+
+})
+
